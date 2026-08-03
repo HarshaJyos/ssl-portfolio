37581f9ba6029a82c08508812699c38480672350
HarshaJyos <harshajyosyabhatla@gmail.com>
2026-08-03 20:38:34 +0530

    perf: configure preload='metadata' on homepage video to minimize bandwidth and storage egress costs

--- src/components/home/AboutUs.tsx
+++ src/components/home/AboutUs.tsx
@@ -89,6 +89,7 @@ export default function AboutUs() {
                   controls
                   autoPlay
                   playsInline
+                  preload="metadata"
                 />
               ) : (
                 <button
